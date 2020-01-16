7c96b39473d99fb147d28746c18efa84b12989c3
Daniel Cordell <danielbc@hotmail.co.uk>
2020-01-16 02:51:51 +0000

    Formatting changes

--- src/main/java/com/abc/Account.java
+++ src/main/java/com/abc/Account.java
@@ -31,13 +31,13 @@ public class Account {
         }
     }
 
-public void withdraw(double amount) {
-    if (amount <= 0) {
-        throw new IllegalArgumentException("amount must be greater than zero");
-    } else {
-        transactions.add(new Transaction(-amount));
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("amount must be greater than zero");
+        } else {
+            transactions.add(new Transaction(-amount));
+        }
     }
-}
 
     public double interestEarned() {
         double amount = sumTransactions();
@@ -45,15 +45,14 @@ public void withdraw(double amount) {
             case SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.001;
-                else
-                    return 1 + (amount-1000) * 0.002;
+                return 1 + (amount-1000) * 0.002;
             case MAXI_SAVINGS:
                 if (amount <= 1000)
                     return amount * 0.02;
                 if (amount <= 2000)
                     return 20 + (amount-1000) * 0.05;
                 return 70 + (amount-2000) * 0.1;
-            default:
+            default: // Checking Account
                 return amount * 0.001;
         }
     }
